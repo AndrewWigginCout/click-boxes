b0619a75f68c8dc4b80f76aa71ea0e03907d601f
= <AndrewWiggin@example.com>
2022-05-24 06:52:15 -0700

    set 30 to red

--- script.js
+++ script.js
@@ -15,7 +15,20 @@ function setCellColour( cell )
 }
 
 
-
+function myfun()
+{
+    console.log("HELLO");
+    var i = document.getElementById("30");
+    i.style.backgroundColor = "red";
+}
+function draw-from-list(v)
+{
+    for ( var i=0; i < v.length; i+=2){
+      var y = v[i];
+      var x = v[i+1];
+      var cell = document.getElementById(String(y*gridSize+x));
+      cell.style.backgroundColor = "red";}
+}
 
 html = "";
 
@@ -33,7 +46,7 @@ for ( var i = 0; i < gridSize; i++ ) { //creates grid Column with limit of gridz
             //html += `<div class="cell" onclick="setCellColour(this)">${char}</div>`;
             //(i, j) grid coordinates;
             
-            html += `<div class="cell" onclick="setCellColour(this)">(${i}, ${j})</div>`;
+            html += `<div id=${i*gridSize+j} class="cell" onclick="setCellColour(this)">(${i}, ${j})</div>`;
             
             // Blank cells
     //         html += `<div class="cell" onclick="setCellColour(this)"></div>`;
@@ -43,4 +56,5 @@ for ( var i = 0; i < gridSize; i++ ) { //creates grid Column with limit of gridz
   
 
 }
+html += '<button onclick="myfun()">myfun</button>';
 board.innerHTML = html;
